02be3d5a651cd6fa7f84df41035599e5e50e30bd
Shipulin1990 <168572651+Shipulin1990@users.noreply.github.com>
2024-07-02 20:50:54 +0300

    Update homework5-3.py

--- homework5-3.py
+++ homework5-3.py
@@ -10,30 +10,39 @@ class House:
     #    return (self.name, self.number_of_floors)
 
     def __eq__(self, other):
-        if isinstance(other, House):
-            return (self.number_of_floors == self.number_of_floors)
+        return self.number_of_floors == other.number_of_floors and self.name == other.name
 
     def __add__(self, value):
-        if isinstance(int(self.number_of_floors), int):
-            return House (int(self.number_of_floors), int)
-        return NotImplemented
+        self.number_of_floors += value
+        return self
 
     def __iadd__(self, value):
-        if isinstance(int(self.number_of_floors), int):
-            return House (int(self.number_of_floors), int)
-        return NotImplemented
+        return self.__add__(value)
 
     def __radd__(self, value):
-        if isinstance(int(self.number_of_floors), int):
-            return House (int, int(self.number_of_floors))
-        return NotImplemented
+        return self.__add__(value)
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 h3 = House('ЖК Платан', 20)
 print(str(f'Название: {h1.name}, кол-во этажей: {h1.number_of_floors}'))
 print(str(f'Название: {h2.name}, кол-во этажей: {h2.number_of_floors}'))
-print(h1 == h2) # Метод eq - выдает True, а не False.  Не пойму почему ошибка?
+print(h1 == h2)
 h3 = h1 + 10
 print(h3) # С этого момента какие-то результаты получаются, но что это значит?
 print(h1 == h3)
@@ -42,8 +51,8 @@ print(h1)
 h2 = 10 + h2
 print(h2)
 
-print(h1 > h2)
-print(h1 >= h2)
 print(h1 < h2)
 print(h1 <= h2)
+print(h1 > h2)
+print(h1 >= h2)
 print(h1 != h2)
